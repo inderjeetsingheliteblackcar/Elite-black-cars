407407b317328d547f56740df06f7446cab85535
inderjeetsingheliteblackcar <inderjeetsingheliteblackcar@gmail.com>
2025-05-13 00:12:22 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,10 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="M_wYaV_WQaXzDlBStbdhT2zr_kFrQ_wGZOwQekhF_Ck" />
       <link rel="icon" type="image/png" href="/images/Logo_main1.svg" />
-        Meta Title:- Eliteb Car offers luxury chauffeur services for corporate events, airport transfers, weddings & more. Experience comfort, class & elegance.
-Meta Keywords:- luxury car rental, wedding car hire, chauffeur service, elite car rentals, Exclusive car service NYC, LGA black car service, Long Island limo service, NYC airport executive ride, Elite luxury vehicle transport.
+        Meta Title:- Elite B Car – Luxury Chauffeur NYC, Events & Corporate Travel.
+        Meta Descriptions:- Eliteb Car offers luxury chauffeur services for corporate events, airport transfers, weddings & more. Experience comfort, class & elegance.
+        Meta Keywords:- luxury car rental, wedding car hire, chauffeur service, elite car rentals, Exclusive car service NYC, LGA black car service, Long Island limo service, NYC airport executive ride, 
+                        Elite luxury vehicle transport.
       </head>
       <ReactLenis root>
         <body
