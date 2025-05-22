1fda489850ce288360671a12531914c92f0dc2dd
Gajanand Sokhal <gajanandsokhal0707@gmail.com>
2025-05-22 10:20:55 +0530

    blog error fixing

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,19 +7,22 @@ interface BlogPageProps {
 }
 
 export function generateStaticParams() {
-  return blogs.map((post) => ({
+  return  blogs.map((post) =>({
     slug: post.slug,
   }));
 }
 
 export default async function BlogPost({ params }: BlogPageProps) {
-  const { slug } = await params; // ✅ FIXED: no 
+  const { slug } = await Promise.resolve(params); 
   const post = blogs.find((item) => item.slug === slug);
 
   if (!post) {
-    return <p className="p-10 text-red-600">Blog post not found.</p>;
+    return (
+      <div className="p-10 text-red-600">
+        Blog post not found.
+      </div>
+    );
   }
-
   return (
     <div className="p-6 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
