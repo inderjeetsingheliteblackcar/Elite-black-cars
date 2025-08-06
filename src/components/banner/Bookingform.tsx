9f0b6a4ad55f08077c5fdbf674dbf5dd4551b81b
inderjeetsingheliteblackcar <inderjeetsingheliteblackcar@gmail.com>
2025-08-06 09:46:42 +0530

    Update Bookingform.tsx

--- src/components/banner/Bookingform.tsx
+++ src/components/banner/Bookingform.tsx
@@ -62,7 +62,7 @@ const Bookingform = () => {
                     href="https://book.mylimobiz.com/v4/eliteblackcar"
                     data-ores-widget="quickres"
                     data-ores-alias="eliteblackcar"
-                    data-redirect-url="http://widgets.la-examples.com/book.php"
+                    data-redirect-url="https://book.mylimobiz.com/v4/eliteblackcar"
                 >
                     Online Reservations
                 </a>
