e6ab5bfecedda1b2af58c43eebc75e57f1dcf711
Gajanand Sokhal <gajanandsokhal0707@gmail.com>
2025-08-23 11:06:50 +0530

    blogs

--- src/components/blogform/Bormblog.js
+++ src/components/blogform/Bormblog.js
@@ -54,7 +54,7 @@ export default function Bormblog() {
       }
 
       // Save to Airtable (via your API route)
-      const airtableResponse = await axios.post('/api/upload', {
+      const airtableResponse = await axios.post('@/app/blog/route', {
         name: formData.name,
         description: formData.description,
         content: formData.content,
