bef163528221a29e0792d85b692aee19e0ad688b
inderjeetsingheliteblackcar <inderjeetsingheliteblackcar@gmail.com>
2025-08-23 14:36:47 +0530

    Update Bormblog.js

--- src/components/blogform/Bormblog.js
+++ src/components/blogform/Bormblog.js
@@ -54,7 +54,7 @@ export default function Bormblog() {
       }
 
       // Save to Airtable (via your API route)
-      const airtableResponse = await axios.post('@/app/blog', {
+      const airtableResponse = await axios.post('/api/blog', {
         name: formData.name,
         description: formData.description,
         content: formData.content,
