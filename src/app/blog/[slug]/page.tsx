12b14194a7bdced29241df9c28b51fca8e48037f
Gajanand Sokhal <gajanandsokhal0707@gmail.com>
2025-08-25 12:02:10 +0530

    spacing blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -45,7 +45,7 @@ export default async function BlogPost({
   }
 
   return (
-    <div className="pt-40 pb-10 md:pb-20 px-4 max-w-5xl mx-auto">
+    <div className="pt-40 blogs-spacing pb-10 md:pb-20 px-4 max-w-5xl mx-auto">
       <h1 className="text-4xl font-bold mb-6">{post.fields.Name}</h1>
 
       {post.fields.Image?.[0]?.url && (
